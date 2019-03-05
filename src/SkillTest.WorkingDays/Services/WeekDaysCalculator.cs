008fe98800329f113255aacb7d2155fe9262d71b
Fergal O'Donnell <fergal.odonnell@gmail.com>
2019-03-05 19:40:07 +1100

    Added a working day calculator

---
+++ src/SkillTest.WorkingDays/Services/WeekDaysCalculator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace SkillTest.WorkingDays.Services
+{
+    public class WeekDaysCalculator
+    {
+        public int Calculate(DateTime fromDate, DateTime toDate)
+        {
+            if (!CrossesWeekend(fromDate, toDate))
+            {
+                var result = (toDate - fromDate).Days - 1;
+                return result > 0 ? result : 0;
+            }
+
+            var fromEndOfWeek = SnapToEndOfWeek(fromDate);
+            var toStartOfWeek = SnapToStartOfWeek(toDate);
+
+            var total = (toStartOfWeek - fromEndOfWeek).Days;
+            var weekendDays = ((total / 7)) * 2;
+            var daysUntilNextWeekend = IsWeekend(fromDate) ? 0 : (fromEndOfWeek - fromDate).Days - 2;
+            var daysToPrevWeekend = IsWeekend(toDate) ? 0 : (toDate - toStartOfWeek).Days - 1;
+
+            return total - weekendDays + daysUntilNextWeekend + daysToPrevWeekend;
+        }
+
+        private bool IsWeekend(DateTime date)
+            => date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday;
+
+        private bool CrossesWeekend(DateTime fromDate, DateTime toDate) =>
+            (toDate - fromDate).Days > 6 || fromDate.DayOfWeek > toDate.DayOfWeek;
+
+        private DateTime SnapToStartOfWeek(DateTime date)
+        {
+            if (date.DayOfWeek == DayOfWeek.Saturday)
+            {
+                return  date.AddDays(1);
+            }
+
+            var diff = Math.Abs((int) DayOfWeek.Sunday - (int) date.DayOfWeek);
+            return date.AddDays(diff * -1);
+        }
+
+        private DateTime SnapToEndOfWeek(DateTime date)
+        {
+            if (date.DayOfWeek == DayOfWeek.Saturday)
+            {
+                return date.AddDays(1);
+            }
+
+            var diff = ((int)DayOfWeek.Sunday - (int)date.DayOfWeek + 7) % 7;
+            return date.AddDays(diff);
+        }
+    }
+}
---
+++ test/SkillTest.WorkingDays.Test.Unit/Services/WeekDaysCalculatorTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using FluentAssertions;
+using SkillTest.WorkingDays.Services;
+using Xunit;
+
+namespace SkillTest.WorkingDays.Test.Unit.Services
+{
+    public class WeekDaysCalculatorTests
+    {
+        private readonly WeekDaysCalculator _target;
+
+        public WeekDaysCalculatorTests()
+        {
+            _target = new WeekDaysCalculator();
+        }
+
+        [Theory]
+        [InlineData("2019-03-03", "2019-03-09", 5)]
+        [InlineData("2019-03-02", "2019-03-09", 5)]
+        [InlineData("2019-03-02", "2019-03-10", 5)]
+        [InlineData("2019-03-03", "2019-03-23", 15)]
+        [InlineData("2019-03-03", "2019-03-30", 20)]
+        [InlineData("2019-03-03", "2019-03-31", 20)]
+        public void Can_Calculate_Work_Days_Between_Weekends(DateTime fromDate, DateTime toDate, int expected)
+        {
+            _target.Calculate(fromDate, toDate).Should().Be(expected);
+        }
+
+        [Fact]
+        public void Can_Calculate_Work_Days_Until_Weekend()
+        {
+            var midweek = new DateTime(2019, 03, 05);
+            var weekend = new DateTime(2019, 03, 10);
+
+            _target.Calculate(midweek, weekend).Should().Be(3);
+        }
+
+        [Fact]
+        public void Can_Calculate_Work_Days_After_Weekend()
+        {
+            var weekend = new DateTime(2019, 03, 10);
+            var midweek = new DateTime(2019, 03, 14);
+
+            _target.Calculate(weekend, midweek).Should().Be(3);
+        }
+
+        [Theory]
+        [InlineData("2019-03-04", "2019-03-08", 3)]
+        [InlineData("2019-03-04", "2019-03-05", 0)]
+        [InlineData("2019-03-04", "2019-03-04", 0)]
+        public void Can_Calculate_Within_Same_Week(DateTime fromDate, DateTime toDate, int expected)
+        {
+            _target.Calculate(fromDate, toDate).Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData("2014-08-7", "2014-08-11", 1)]
+        [InlineData("2014-08-13", "2014-08-21", 5)]
+        [InlineData("2019-03-01", "2019-03-31", 20)]
+        public void Can_Calculate_Work_Days_Between_Range(DateTime fromDate, DateTime toDate, int total)
+        {
+            _target.Calculate(fromDate, toDate).Should().Be(total);
+        }
+    }
+}
